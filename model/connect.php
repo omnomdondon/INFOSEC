bb07033a90abfc68573359a6999f34338bcc41ad
Brandon Kenneth Cinco <brandon1203kennethdc@gmail.com>
2025-03-01 00:20:26 +0800

    INITIAL COMMIT

--- model/connect.php
+++ model/connect.php
@@ -1,15 +1,15 @@
 <?php
-$HOST = 'localhost';
-$USER = 'root';
-$PASSWORD = '';
-$DB = 'blog_db';
-$PORT = 3306;
-
 // $HOST = 'localhost';
 // $USER = 'root';
-// $PASSWORD = 'DonBelle_111522';
+// $PASSWORD = '';
 // $DB = 'blog_db';
-// $PORT = 3307;
+// $PORT = 3306;
+
+$HOST = 'localhost';
+$USER = 'root';
+$PASSWORD = 'DonBelle_111522';
+$DB = 'blog_db';
+$PORT = 3307;
 
 $CONN = new mysqli($HOST, $USER, $PASSWORD, $DB, $PORT);
 if ($CONN->connect_error) {
